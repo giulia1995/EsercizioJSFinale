71d54f3b23d9baf4e93621fc7299e29ea7a3cc7b
Giulia_vi95 <giulia.vaccaro95@gmail.com>
2023-11-02 23:07:23 +0100

    last version

--- javascriptEsercizioFinale.js
+++ javascriptEsercizioFinale.js
@@ -37,150 +37,173 @@ Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati 
 
 // NON MODIFICARE QUESTO ARRAY!
 const jobs = [
-    { title: "Marketing Intern", location: "US, NY, New York" },
-    {
-      title: "Customer Service - Cloud Video Production",
-      location: "NZ, Auckland",
-    },
-    {
-      title: "Commissioning Machinery Assistant (CMA)",
-      location: "US, IA, Wever",
-    },
-    {
-      title: "Account Executive - Washington DC",
-      location: "US, DC, Washington",
-    },
-    { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
-    { title: "Accounting Clerk", location: "US, MD," },
-    { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
-    {
-      title: "Lead Guest Service Specialist",
-      location: "US, CA, San Francisco",
-    },
-    { title: "HP BSM SME", location: "US, FL, Pensacola" },
-    {
-      title: "Customer Service Associate - Part Time",
-      location: "US, AZ, Phoenix",
-    },
-    {
-      title: "ASP.net Developer Job opportunity at United States,New Jersey",
-      location: "US, NJ, Jersey City",
-    },
-    {
-      title: "Talent Sourcer (6 months fixed-term contract)",
-      location: "GB, LND, London",
-    },
-    {
-      title: "Applications Developer, Digital",
-      location: "US, CT, Stamford",
-    },
-    { title: "Installers", location: "US, FL, Orlando" },
-    { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
-    {
-      title: "VP of Sales - Vault Dragon",
-      location: "SG, 01, Singapore",
-    },
-    { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
-    {
-      title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
-      location: "GB, SOS, Southend-on-Sea",
-    },
-    { title: "Visual Designer", location: "US, NY, New York" },
-    {
-      title: "Process Controls Engineer - DCS PLC MS Office - PA",
-      location: "US, PA, USA Northeast",
-    },
-    { title: "Marketing Assistant", location: "US, TX, Austin" },
-    { title: "Front End Developer", location: "NZ, N, Auckland" },
-    { title: "Engagement Manager", location: "AE," },
-    {
-      title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
-      location: "US, CA, Carlsbad",
-    },
-    { title: "Customer Service", location: "GB, LND, London" },
-    { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
-    { title: "Marketing Exec", location: "SG," },
-    {
-      title: "HAAD/DHA Licensed Doctors Opening in UAE",
-      location: "AE, AZ, Abudhabi",
-    },
-    {
-      title: "Talent Management Process Manager",
-      location: "US, MO, St. Louis",
-    },
-    { title: "Customer Service Associate", location: "CA, ON, Toronto" },
-    {
-      title: "Customer Service Technical Specialist",
-      location: "US, MA, Waltham",
-    },
-    { title: "Software Applications Specialist", location: "US, KS," },
-    { title: "Craftsman Associate", location: "US, WA, Everett" },
-    { title: "Completion Engineer", location: "US, CA, San Ramon" },
-    { title: "I Want To Work At Karmarama", location: "GB, LND," },
-    {
-      title: "English Teacher Abroad",
-      location: "US, NY, Saint Bonaventure",
-    },
-  ]
-     // ricerca lavori
-     function searchJobs(title, location) {
-      console.log("Ricerca in corso...");
-  
-      title = title.toLowerCase();
-      location = location.toLowerCase();
-  
-      const results = [];
-      for (let i = 0; i < jobs.length; i++) {
-        const job = jobs[i];
-        const jobTitle = job.title.toLowerCase();
-        const jobLocation = job.location.toLowerCase();
-        if (jobTitle.includes(title) && jobLocation.includes(location)) {
-          results.push(job);
-        }
-      }
-  
-      console.log("Risultati della ricerca:", results);
-      return results;
+  { title: "Marketing Intern", location: "US, NY, New York" },
+  {
+    title: "Customer Service - Cloud Video Production",
+    location: "NZ, Auckland",
+  },
+  {
+    title: "Commissioning Machinery Assistant (CMA)",
+    location: "US, IA, Wever",
+  },
+  {
+    title: "Account Executive - Washington DC",
+    location: "US, DC, Washington",
+  },
+  { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
+  { title: "Accounting Clerk", location: "US, MD," },
+  { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
+  {
+    title: "Lead Guest Service Specialist",
+    location: "US, CA, San Francisco",
+  },
+  { title: "HP BSM SME", location: "US, FL, Pensacola" },
+  {
+    title: "Customer Service Associate - Part Time",
+    location: "US, AZ, Phoenix",
+  },
+  {
+    title: "ASP.net Developer Job opportunity at United States,New Jersey",
+    location: "US, NJ, Jersey City",
+  },
+  {
+    title: "Talent Sourcer (6 months fixed-term contract)",
+    location: "GB, LND, London",
+  },
+  {
+    title: "Applications Developer, Digital",
+    location: "US, CT, Stamford",
+  },
+  { title: "Installers", location: "US, FL, Orlando" },
+  { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
+  {
+    title: "VP of Sales - Vault Dragon",
+    location: "SG, 01, Singapore",
+  },
+  { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
+  {
+    title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
+    location: "GB, SOS, Southend-on-Sea",
+  },
+  { title: "Visual Designer", location: "US, NY, New York" },
+  {
+    title: "Process Controls Engineer - DCS PLC MS Office - PA",
+    location: "US, PA, USA Northeast",
+  },
+  { title: "Marketing Assistant", location: "US, TX, Austin" },
+  { title: "Front End Developer", location: "NZ, N, Auckland" },
+  { title: "Engagement Manager", location: "AE," },
+  {
+    title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
+    location: "US, CA, Carlsbad",
+  },
+  { title: "Customer Service", location: "GB, LND, London" },
+  { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
+  { title: "Marketing Exec", location: "SG," },
+  {
+    title: "HAAD/DHA Licensed Doctors Opening in UAE",
+    location: "AE, AZ, Abudhabi",
+  },
+  {
+    title: "Talent Management Process Manager",
+    location: "US, MO, St. Louis",
+  },
+  { title: "Customer Service Associate", location: "CA, ON, Toronto" },
+  {
+    title: "Customer Service Technical Specialist",
+    location: "US, MA, Waltham",
+  },
+  { title: "Software Applications Specialist", location: "US, KS," },
+  { title: "Craftsman Associate", location: "US, WA, Everett" },
+  { title: "Completion Engineer", location: "US, CA, San Ramon" },
+  { title: "I Want To Work At Karmarama", location: "GB, LND," },
+  {
+    title: "English Teacher Abroad",
+    location: "US, NY, Saint Bonaventure",
+  },
+];
+// funzione per inserire i lavori nella ricerca e rendo la funzione case insensitive
+function searchJobs(title, location) {
+  console.log("Ricerca");
+
+  title = title.toLowerCase();
+  location = location.toLowerCase();
+
+  const results = [];
+  for (let i = 0; i < jobs.length; i++) {
+    const job = jobs[i];
+    const jobTitle = job.title.toLowerCase();
+    const jobLocation = job.location.toLowerCase();
+    if (jobTitle.includes(title) && jobLocation.includes(location)) {
+      results.push(job);
     }
-  
-    // alert quando non si compilano entrambi i campi
-    function showAlert(message) {
-      alert(message);
-    }
-  
-    // mostro i risultati nella pagina
-    function displayResults() {
-      console.log("Visualizzazione dei risultati...");
+  }
 
-      const titleInput = document.getElementById("titleInput");
-      const locationInput = document.getElementById("locationInput");
-      const resultsList = document.getElementById("resultsList");
-      const resultsCount = document.getElementById("resultsCount"); // elemento counter
+  console.log("Research results:", results);
+  return results;
+}
 
-      // Verifico se entrambi gli input sono completi
-      if (titleInput.value.trim() === "" || locationInput.value.trim() === "") {
-        showAlert("Enter both your job title and geographic location.");
-        return;
-      }
+// mostro i risultati nella pagina
+function displayResults() {
+  console.log("Show Results");
 
-      const results = searchJobs(titleInput.value, locationInput.value);
-      const count = results.length; // counter dei risultati
+//Dichiaro le costanti e richiamo gli elementi
 
-      console.log("Numero di inserzioni trovate:", count);
-      resultsCount.textContent = `Jobs Result: ${count}`; // Mostra il conteggio
+  const titleInput = document.getElementById("titleInput");
+  const locationInput = document.getElementById("locationInput");
+  const resultsList = document.getElementById("resultsList");
 
-      // Svuoto lista risultati
-      resultsList.innerHTML = "";
+// Verifico se entrambi gli input sono completi altrimenti alert
+  if (titleInput.value.trim() === "" || locationInput.value.trim() === "") {
+    showAlert("Enter both your job title and geographic location.");
+    return;
+  }
+  /*.trim() lo utilizzo (alla fine altrimenti non va!) 
+  per fare in modo che se l'utente inserisce spazi vuoti, 
+  questo non influenza la ricerca ed inoltre mi permette di evitare
+  la ricerca se non sono completi entrambi i campi altrimenti ALERT che blocca la ricerca
+  */
+  function showAlert(message) {
+    alert(message);
+  }
+  const resultsCount = document.getElementById("resultsCount"); // elemento counter
+  const results = searchJobs(titleInput.value, locationInput.value);
+  const count = results.length; // counter dei risultati
+
+  console.log("Jobs found:", count);
+  resultsCount.textContent = `Jobs Result: ${count}`; // Mostro il counter
+
+  // Qui svuoto la lista dei risultati
+  resultsList.innerHTML = "";
+
+  /*
+      Qui avevo usato append.Child per completare la funzione e mostrare i risultati
 
       for (let i = 0; i < count; i++) {
         const job = results[i];
         const listItem = document.createElement("li");
-        listItem.textContent = `${job.title} - ${job.location}`;
+        listItem.innerText = `${job.title} | ${job.location}`;
         resultsList.appendChild(listItem);
       }
       titleInput.value = "";
       locationInput.value = "";
     }
+    
+     Alla fine ho usato innerHTML che effettivamente era anche più breve!
+     count è dichiarata sopra per creare il counter inserzioni
+  */
 
-    const searchButton = document.getElementById("searchButton");
-    searchButton.addEventListener("click", displayResults);
+  let resultsHTML = ""; 
+  for (let i = 0; i < count; i++) {
+    const job = results[i];
+    resultsHTML += `<li>${job.title} - ${job.location}</li>`;
+  }
+  resultsList.innerHTML = resultsHTML;
+  //Svuoto i campi input
+  titleInput.value = "";
+  locationInput.value = "";
+}
+
+//Evento che scatena al click la ricerca e fa vedere i risultati
+const searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", displayResults);
